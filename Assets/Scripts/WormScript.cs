e5e58294e73075c67cce4818cdb1ab2ee876a32d
Austin Lancaster <aus@lancaster@gmail.com>
2019-07-09 19:42:23 +1000

    3rd iteration of worm
    Worm propelled by head and tail. Chooses tail only if the head is a certain distance closer to the objective. Random angle offset added to each propulsion.

--- Assets/Scripts/WormScript.cs
+++ Assets/Scripts/WormScript.cs
@@ -7,6 +7,10 @@ public class WormScript : MonoBehaviour
     private const int PUSH_DURATION = 16;
     private const int WAITS_TO_PUSHES = 3;
     private const float PUSH_FORCE = 60;
+    private const float MOV_ANGLE_OFFSET = 60f;
+
+    [SerializeField]
+    float minHeadTailSeparation = 4f;
 
     [SerializeField]
     private Rigidbody2D headRB;
@@ -18,7 +22,10 @@ public class WormScript : MonoBehaviour
     private Transform tailTransform;
     private List<Rigidbody2D> segmentRbs;
     private List<Transform> segmentTransforms;
+    private Transform selectedTransform;
+    private Rigidbody2D selectedRB;
     private static int steps;
+    private float offsetAngle;
 
     // Start is called before the first frame update
     void Start()
@@ -36,8 +43,43 @@ public class WormScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        int instruction = (steps / PUSH_DURATION) % (WAITS_TO_PUSHES);
+        int instruction = (steps / PUSH_DURATION) % WAITS_TO_PUSHES;
         if (instruction == 0)
+        {
+            if (steps % PUSH_DURATION == 0)
+            {
+                // pick which segment to push with
+
+                float headDist = GetDirectionToMouse(new Vector2(headTransform.position.x, headTransform.position.y), false).magnitude;
+                float tailDist = GetDirectionToMouse(new Vector2(tailTransform.position.x, tailTransform.position.y), false).magnitude;
+
+                if (tailDist - headDist < minHeadTailSeparation)
+                {
+                    // choose head
+                    selectedTransform = headTransform;
+                    selectedRB = headRB;
+                }
+                else
+                {
+                    // choose tail
+                    selectedTransform = tailTransform;
+                    selectedRB = tailRB;
+                }
+
+                offsetAngle = UnityEngine.Random.Range(-MOV_ANGLE_OFFSET, MOV_ANGLE_OFFSET);
+
+            }
+
+
+            // push chosen segment
+            Vector2 dir = GetDirectionToMouse(new Vector2(selectedTransform.position.x, selectedTransform.position.y));
+            dir = Vector2Extension.Rotate(dir, offsetAngle);
+            selectedRB.AddForce((PUSH_FORCE * 0.5f) * dir);
+        }
+
+
+
+        /*if (instruction == 0)
         {
             // pick random segment
             int segmentId = UnityEngine.Random.Range(0, segmentTransforms.Count);
@@ -46,16 +88,22 @@ public class WormScript : MonoBehaviour
             // push segment
             Vector2 dir = GetDirectionToMouse(new Vector2(v.x, v.y));
             rb.AddForce(PUSH_FORCE * dir);
-        }
+        }*/
+
 
 
         steps++;
     }
-
-    private Vector2 GetDirectionToMouse(Vector2 from)
+    
+    private Vector2 GetDirectionToMouse(Vector2 from, bool normalized = true)
     {
         Vector3 v = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        return (new Vector2(v.x, v.y) - from).normalized;
+        Vector2 v2 = new Vector2(v.x, v.y) - from;
+        if (normalized)
+        {
+            v2 = v2.normalized;
+        }
+        return v2;
 
         //return Camera.main.ScreenToWorldPoint(Input.mousePosition);
     }
